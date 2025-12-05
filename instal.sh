8939a7dbb8787e777638ff7a3af8e1a1bdcf6e2f
Gabriel6223684 <gabrielgb.luiz000@gmail.com>
2025-12-04 21:25:39 -0400

    Commit

--- instal.sh
+++ instal.sh
@@ -10,4 +10,11 @@ composer update --no-dev --no-progress -a
 composer upgrade --no-dev --no-progress -a
 composer do -o --no-dev --no-progress -a
 
+PG_USER="senac"
+PG_PASS="senac"
+PG_DB="gabrielluiz"
+PG_HOST="localhost"
+PG_PORT="5432"
+
+
 service nginx reload
